1396e3717517b9110220c0cbd25c69afac143ffe
Sahil <sahilashokjagdale@gmail.com>
2025-03-10 15:38:39 +0530

    init commit

--- Java_00/basics.java
+++ Java_00/basics.java
@@ -1,5 +1,17 @@
 public class basics {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        // System.out.println("Hello world");
+        for(int i=0; i<=15; i++) {
+            System.out.println("I love you");
+        }
+        int i = 1;
+        while(i<=5) {
+            System.out.println("hello world");
+            i++;
+        }
+        int cond = 15;
+        int res = 0;
+        res = cond>res ? 10 : 15;
+        System.out.println(res);
     }
 }
